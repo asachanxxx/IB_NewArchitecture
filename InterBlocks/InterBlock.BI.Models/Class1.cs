e1410e0542a918d1d0c775ebd1c97555e902a89a
asachanxxx <cspl.itm@googlemail.com>
2019-10-14 23:15:05 +0530

    Fresh start

---
+++ InterBlocks/InterBlock.BI.Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterBlock.BI.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ InterBlocks/InterBlock.Repositories.Base/test.cs
@@ -0,0 +1,33 @@
+﻿using InterBlock.DataEngine.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterBlock.Repositories.Base
+{
+    public class test
+    {
+        public test()
+        {
+            DBContext context = new DBContext(Helpers.Enums.DataBaseType.MSSql,new Helpers.Configurations.Connections(""));
+            var type = context._DbType;
+            var connection = context._Connection;
+
+            //Usage
+            var result = context.QueryProcessor.ExcuteStoredProcedureToSave<TestClass>("MySPName", new TestClass() { Id = 1, ValueString = "Test", ValueDecimal = 1234 });
+
+            
+        }
+
+
+    }
+
+    public class TestClass {
+        public int Id { get; set; }
+        public string ValueString { get; set; }
+        public decimal ValueDecimal { get; set; }
+
+    }
+}
