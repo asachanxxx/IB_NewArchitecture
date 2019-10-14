29f940d6227e2759f5d03de1687a33307bfc917b
asachanxxx <cspl.itm@googlemail.com>
2019-10-15 00:00:47 +0530

    asd

---
+++ InterBlocks/InterBlock.Repositories.Base/ExtendedRepository.cs
@@ -0,0 +1,61 @@
+﻿using InterBlock.DataEngine.Common;
+using InterBlock.Helpers.Configurations;
+using InterBlock.Helpers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterBlock.Repositories.Base
+{
+    public class ExtendedRepository<T> : DBContextExtendedBase, iRepositoryBase<T> where T : class
+    {
+        private IBConfiguration _config;
+
+        public ExtendedRepository(IBConfiguration config):base(config)
+        {
+            _config = config;
+        }
+
+        public Task<bool> DeleteMulti(T Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteSingle(T Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteSingle(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> FindALLRecords()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> FindOneRecord(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ModifySingle(T Entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveMulti(T Entity, int Action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveSingle(T Entity, int Action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- InterBlocks/InterBlock.Repositories.Base/test.cs
+++ InterBlocks/InterBlock.Repositories.Base/test.cs
@@ -18,6 +18,7 @@ namespace InterBlock.Repositories.Base
             //Usage
             var result = context.QueryProcessor.ExcuteStoredProcedureToSave<TestClass>("MySPName", new TestClass() { Id = 1, ValueString = "Test", ValueDecimal = 1234 });
 
+            ExtendedRepository<TestClass> db = new ExtendedRepository<TestClass>(new Helpers.Configurations.IBConfiguration() { Connection =  new Helpers.Configurations.Connections("Constr") , DbType = Helpers.Enums.DataBaseType.MSSql });
             
         }
 
